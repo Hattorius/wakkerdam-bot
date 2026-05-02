e53acfb365d9f7cdb3787f9b90a20885c2706202
Aaron Jonk <AJ@jonk.name>
2026-05-02 22:39:04 +0200

    env

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log/slog"
+import (
+	"log/slog"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		slog.Error("Failed loading environmental variables from .env file", "error", err)
+		os.Exit(1)
+	}
+
 	slog.Info("Selam")
 }
